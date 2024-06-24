ce2b483679288757a68e7e55c8ed2c531f345e0f
Frantisek B <frantisekbabinsky.cz@gmail.com>
2024-06-24 12:05:13 +0200

    updated app.js

--- js/app.js
+++ js/app.js
@@ -70,3 +70,5 @@ paragraph.setAttribute("class", "light");
 console.log(paragraph.getAttribute("class"));
 paragraph.classList.add("light");
 console.log(paragraph.style.cssText);
+
+//end of the sesion 28, ATR10
