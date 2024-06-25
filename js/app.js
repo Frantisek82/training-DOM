3337890bf090ccc47b440fc4d2ee0bbeec4e8612
Frantisek B <frantisekbabinsky.cz@gmail.com>
2024-06-25 12:17:38 +0200

    updated dark-theme

--- js/app.js
+++ js/app.js
@@ -9,19 +9,26 @@ document.addEventListener("DOMContentLoaded", () => {
     icon.classList.toggle("fa-sun");
     icon.classList.toggle("fa-moon");
     icon.classList.toggle("spin");
+    icon.classList.toggle("spin");
     icon.classList.toggle("buzz-out");
     header.classList.toggle("smack");
-    if ((header.textContent = "Good Morning!")) {
-      // if (header.textContent.includes("Good Morning!")){ // alternative version
+    header.classList.toggle("buzz-out");
+    header.classList.toggle("buzz-out");
+    if (header.textContent == "Good Morning!") {
+      // if (header.textContent.includes("Good Morning!")){ // alternative version of the code
+      //   header.textContent = "Good Night!"; // alternative version of the code
       header.textContent = header.innerHTML.replace(
         "Good Morning!",
         "Good Night!"
       );
+      console.log(header.textContent);
     } else {
+      //   header.textContent = "Good Morning!"; // alternative version of the code
       header.textContent = header.innerHTML.replace(
         "Good Night!",
         "Good Morning!"
       );
+      console.log(header.textContent);
     }
   });
 });
